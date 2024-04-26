0a7c15f7d3425b827972f030784a17b58f9dc0ac
Gedion Zeyede <gediizeyuu@gmail.com>
2024-04-26 18:38:21 +0300

    Time: 441 ms (94.05%), Space: 31 MB (70.88%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         right = len(arr)-1
         
-        while left < right:
+        while left <= right:
             mid = (left + right)//2
 
             if arr[mid-1] < arr[mid] and arr[mid] > arr[mid+1]:
@@ -12,5 +12,6 @@ class Solution:
                 left = mid + 1
             else:
                 right = mid - 1 
-        return right        
+           
+
       
