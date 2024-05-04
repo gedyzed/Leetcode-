f340c5b7d3f76e613ae14fae402de2c103bde6b2
Gedion Zeyede <gediizeyuu@gmail.com>
2024-05-04 14:31:37 +0300

    Time: 49 ms (5.84%), Space: 17 MB (14.84%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -3,39 +3,35 @@ class Solution:
         length = len(s)
         start = 0
         end = k-1
-        holder = 0 #holdes the interval 
+        holder = 0
         s = list(s)
         status = False
      
-        while start <= end :
+        while start <= end and length :
             if k*2 <= length:
                 s[start], s[end] = s[end], s[start]
-                 
+                print(start,end)      
                 if start == end-1 or start == end :
                     holder += k*2
                     start = holder
                     end = start + k-1
                     length = length - k*2
-                    if length == 0:
-                        break      
-                else:
-                    start += 1
-                    end -= 1 
+                    continue
+                start += 1
+                end -= 1 
             elif k*2 > length and length >= k:
                 s[start], s[end] = s[end], s[start]
                 if start == end-1:
                     break
-                else:
-                    start += 1
-                    end -= 1     
-            elif k > length :
-              
+                start += 1
+                end -= 1     
+            else: 
                 if not status:    
                     end = end+length-k
                     status = True
                 s[start], s[end] = s[end], s[start]
                 start += 1
                 end -= 1   
-   
+      
         return "".join(s)
         
