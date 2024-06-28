a036a38d48da9b3e07dcd699a59f7b715da25870
Gedion Zeyede <gediizeyuu@gmail.com>
2024-06-28 18:53:16 +0300

    Time: 29 ms (89.18%), Space: 16.4 MB (98.93%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
-        digits = [digit for digit in str(num) ]
-        print(digits)
 
+        digits = list(str(num)) 
+    
         beautyNumbers = 0
         currentdigits = ""
         digitSum = ""
@@ -12,14 +12,8 @@ class Solution:
             
             if index >= k-1:
                 currentdigits = currentdigits
-                digitSum = str(currentdigits) 
-                while len(digitSum) != 0 and digitSum[0] == "0":
-                    digitSum = digitSum[1:]  
-                    
-                if len(digitSum): 
-                    digitSum = int(digitSum)     
-                    if num / digitSum == num // digitSum: beautyNumbers += 1 
-                        
+                digitSum = int(currentdigits) 
+                if digitSum and num % digitSum == 0 : beautyNumbers += 1         
                 currentdigits = currentdigits[1:]  
                
 
