c7dee9a478ada4a5ac7552bf70cd73ea66601871
Gedion Zeyede <gediizeyuu@gmail.com>
2024-06-30 23:37:34 +0300

    Time: 465 ms (39.00%), Space: 29.8 MB (57.44%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        # Create hset for storing previous of k elements...
-        hset = {}
-        # Traverse for all elements of the given array in a for loop...
-        for idx in range(len(nums)):
-            # If duplicate element is present at distance less than equal to k, return true...
-            if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
-                return True
-            hset[nums[idx]] = idx
-        # If no duplicate element is found then return false...
-        return False        
+        hm = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in hm:
+                hm[nums[i]] = i
+                continue
+            else:
+                if abs(i - hm[nums[i]])<=k:
+                    return True
+                hm[nums[i]]=i
+        return False       
