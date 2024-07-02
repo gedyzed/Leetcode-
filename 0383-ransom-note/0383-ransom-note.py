3e6f003e26d1f08f334f524826d6c03683cac90d
Gedion Zeyede <gediizeyuu@gmail.com>
2024-07-02 23:34:40 +0300

    Time: 51 ms (75.62%), Space: 16.9 MB (13.97%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
+        
