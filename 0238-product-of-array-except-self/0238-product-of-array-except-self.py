3401ea097b13f9466fd34f5807da0366e233f141
Gedion Zeyede <gediizeyuu@gmail.com>
2024-07-19 09:05:53 +0300

    Time: 280 ms (29.70%), Space: 26.2 MB (29.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        zeros,prod = 0,1
-        ans = []
-        for num in nums:
-            if num : prod *= num
-            else : zeros += 1
+        n = len(nums)
+        prefix = [1]*(n+1)
+        suffix = [1]*(n+1)
 
-        for num in nums:
-            if zeros :
-                if zeros > 1 : ans.append(0)
-                elif not num : ans.append(prod)    
-                else : ans.append(0)   
-            else : ans.append(int(prod/num))
-        return ans          
+        for left,right in enumerate(range(n-1,-1,-1)):
+            suffix[right] = suffix[right+1] * nums[right]
+            prefix[left] = prefix[left-1] * nums[left]
+
+        ans = [0] * n
+        for i in range(n):
+            ans[i] = prefix[i-1] * suffix[i+1]
+
+        return ans    
+        
+
+ 
 
 
 
