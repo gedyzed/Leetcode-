22cbdcc1a0bc683de8ed0b56671cb926ff2d3d4d
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-10-26 03:10:28 +0300

    Time: 0 ms (100.00%), Space: 16.5 MB (83.05%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,22 +1,17 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
 
-        stack1, stack2 = [], []
-        for char in s:
-            if stack1 and char == '#':
-                stack1.pop()
-            elif char != '#':
-                stack1.append(char) 
+        def remove_chars(st):
+            stack = []
+            for char in st:
+                if stack and char == '#':
+                    stack.pop()
+                elif char != '#':
+                    stack.append(char)    
+            return stack 
+  
+        return remove_chars(s) == remove_chars(t)
 
-        for char in t:
-            if stack2 and char == '#':
-                stack2.pop()
-            elif char != '#':
-                stack2.append(char)
-
-        if ''.join(stack1) == ''.join(stack2):
-            return True
-        return False                
         
 
         
