2b599da58b8731dc41699151edbdfad682587611
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-11-15 19:39:30 +0300

    Time: 5 ms (21.00%) | Memory: 17.1 MB (17.91%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -2,21 +2,20 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         
         count = Counter(arr1)
-        ans = []
+        arr,ans = [], [] 
         left = right = 0
+        for num in arr1:
+            if num not in arr2:
+                arr.append(num)
+        arr.sort()        
+
         while left < len(arr2):
             if count[arr2[left]] > 0:
                 ans.append(arr2[left]) 
                 count[arr2[left]] -= 1
-            else:
-                left += 1 
-        arr = []        
-        for num in arr1:
-            if num not in arr2:
-                arr.append(num)
-        arr.sort()
-        for num in arr:
-            ans.append(num)
+            else: left += 1 
+
+        ans.extend(arr)
               
         return ans
         
