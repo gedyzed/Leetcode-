20678776d87e60a1e950db81f91e30004491dcf2
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-11-15 18:24:46 +0300

    Time: 72 ms (24.46%) | Memory: 27.5 MB (97.42%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -4,15 +4,18 @@ class Solution:
         n = len(skill)
         totalSum = sum(skill)
         skillSum = totalSum * 2 // n
+        count = defaultdict(int)
+        pair_count = 0
         chemistry = 0
-        skill.sort()
-        left, right = 0, n - 1 
 
-        while left < right:
-            if skill[left] + skill[right] != skillSum:
-                return -1
-            chemistry += skill[left] * skill[right]    
-            left += 1
-            right -= 1
-
+        for i in range(n):
+            x = skillSum - skill[i]
+            if x in count and count[x] > 0:   
+                chemistry += x * skill[i]
+                pair_count += 1
+                count[x] -= 1
+            else:
+                count[skill[i]] += 1    
+        if pair_count != n / 2:
+            return -1
         return chemistry            
