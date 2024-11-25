476b56fb920cbc721d6bbb27f8dacbcc7aa842ea
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-11-25 14:40:57 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (21.80%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+
+        stack = []
+        for log in logs:
+            if log == "../" and stack:
+                stack.pop()
+            elif log != "./" and log != "../":
+                stack.append(log)  
+                  
+        return len(stack)        
+        
