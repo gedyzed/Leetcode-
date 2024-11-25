84ad7a6de92681dcc25bacf905745c5fb892ad9f
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-11-25 14:21:06 +0300

    Time: 95 ms (51.16%) | Memory: 17.8 MB (63.52%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+
+        stack = []
+        for c in s:
+            if c != '*':
+                stack.append(c)
+            elif stack:
+                stack.pop()    
+                
+        return "".join(stack)
+
+        
