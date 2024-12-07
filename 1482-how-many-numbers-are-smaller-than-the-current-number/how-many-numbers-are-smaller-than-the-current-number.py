b4d931f997e03b274bd8db597190a2d79602fa72
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-12-07 22:25:56 +0300

    Time: 3 ms (73.40%) | Memory: 17.4 MB (5.50%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        count = Counter(nums)
+        nums_ = list(set(nums))
+        nums_.sort()
+        min_elements = defaultdict(int)
+        min_elements[nums_[0]] = count[nums_[0]]
+
+        for i in range(1,len(nums_)):
+            min_elements[nums_[i]] += count[nums_[i]] + min_elements[nums_[i - 1]]
+
+        answer = []  
+        for num in nums:
+            answer.append(min_elements[num] - count[num])  
+              
+        return answer     
+
+
+        
