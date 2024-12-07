9b23d4710d7f5915d44bd51b71151d636d1b2615
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-12-07 22:47:07 +0300

    Time: 4 ms (58.88%) | Memory: 17.6 MB (5.71%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,10 +1,23 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
 
+        def countingSort(nums):
+            max_, min_ = max(nums), min(nums)
+            count = [0] * (max_ - min_ + 1)
+
+            for num in nums:
+                count[num - min_] += 1
+
+            sorted_nums = [] 
+            for i, freq in enumerate(count):
+                sorted_nums.extend([i + min_] * freq)     
+
+            return sorted_nums    
+
         count = Counter(nums)
         nums_ = list(set(nums))
-        nums_.sort()
         min_elements = defaultdict(int)
+        nums_ = countingSort(nums_)
         min_elements[nums_[0]] = count[nums_[0]]
 
         for i in range(1,len(nums_)):
@@ -13,8 +26,5 @@ class Solution:
         answer = []  
         for num in nums:
             answer.append(min_elements[num] - count[num])  
-              
-        return answer     
 
-
-        
+        return answer         
