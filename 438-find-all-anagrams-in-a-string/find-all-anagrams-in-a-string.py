d09525f42a0be158557522845dd8c311bc96fa43
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-12-19 12:26:47 +0300

    Time: 76 ms (48.12%) | Memory: 18.5 MB (6.09%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+
+        def check(target, window):
+
+            for k in target.keys():
+                if target[k] != window[k]:
+                    return False
+            return True     
+
+        target = Counter(p)
+        window = defaultdict(int)
+        result = []
+        k = len(p)
+        left = count = 0
+        for right in range(len(s)):
+         
+            if s[right] in target :
+                window[s[right]] += 1
+        
+            if right >= k - 1:  
+                if check(target, window):    
+                    result.append(left) 
+                window[s[left]] -= 1
+                left += 1   
+                  
+        return result  
+
+                 
+                  
+
+
+        
