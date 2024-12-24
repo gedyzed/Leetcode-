708ec93f5dd68f9259dff7b36fe35aa9877e5da4
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2024-12-24 11:42:00 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.00%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -1,9 +1,7 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        stack, index = [], 0
+        stack = []
         popped.reverse()
-        print(popped)
-
         for i in range(len(pushed)):
             stack.append(pushed[i])
             while popped and stack and popped[-1] == stack[-1]:
