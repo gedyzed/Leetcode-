eb83a6e73270ebf29ff3abdd493cd41415ad8d80
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-02 20:40:06 +0300

    Time: 7 ms (81.07%) | Memory: 23.2 MB (14.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,8 +4,16 @@ class Solution:
         for char in s:
             if char.isalnum():
                 result.append(char.lower()) 
-        reversed = result[::-1]         
-        return result == reversed  
+
+        left, right = 0, len(result) - 1  
+        while left < right:
+            if result[left] != result[right]:
+                return False
+            left += 1
+            right -=  1
+
+        return True         
+
 
 
 
