c4823573d53b0de52776e7ca943f61c19410f3d7
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-03 17:12:03 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (28.30%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,13 +1,13 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
 
-        nums1 = set([num for  num in range(left, right + 1)])
         result = []
         for start, end in ranges:
             for num in range(start, end + 1):
                 result.append(num)
-                
-        nums2 = set(result)        
+
+        nums2 = set(result)
+        nums1 = set([num for  num in range(left, right + 1)])        
 
         return nums1 <=  nums2
 
