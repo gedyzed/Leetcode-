f1a0a5773a1885614864c9cff38dd1e32253a5e1
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-04 16:58:47 +0300

    Time: 3 ms (93.81%) | Memory: 17.9 MB (26.68%) - LeetSync

---
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+
+        count_paired = defaultdict(int)
+        for cpdomain in cpdomains:
+            rep, domain = cpdomain.split()
+        
+            sub = domain.split(".")
+            if len(sub) == 3:
+                count_paired[f"{sub[0]}.{sub[1]}.{sub[2]}"] += int(rep)
+                count_paired[f"{sub[1]}.{sub[2]}"] += int(rep)      
+            else:          
+                count_paired[f"{sub[0]}.{sub[1]}"] += int(rep)
+            count_paired[sub[-1]] += int(rep)
+   
+        return [f"{v} {k}" for k, v in count_paired.items()]       
+
+
+
+            
+        
