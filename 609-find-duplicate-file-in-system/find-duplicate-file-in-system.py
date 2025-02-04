e0a72470e5397b865231b27ce7d1383455bcafa4
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-04 14:40:37 +0300

    Time: 23 ms (38.30%) | Memory: 28.4 MB (5.03%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,9 +1,11 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]] :
 
+        for i, path in enumerate(paths):
+            paths[i] = path.split() 
+
         names = []  
-        for i in range(len(paths)):
-            path = paths[i].split()
+        for path in paths:
             for j in range(1, len(path)):
                 names.append(f"{path[0]}/{path[j]}")   
         
