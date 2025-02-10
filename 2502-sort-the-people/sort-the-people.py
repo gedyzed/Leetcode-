75b27520794962d2c0433086efe27ee1af6c3871
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-10 14:49:54 +0300

    Time: 773 ms (5.03%) | Memory: 18.2 MB (41.17%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,21 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        heights_to_names = {}
-        for i in range(len(heights)):
-            heights_to_names[heights[i]] = names[i]
-        max_h = max(heights)
-        min_h = min(heights)
 
-        ans = [0] * (max_h - min_h + 1)
-        for height in heights :
-            ans[height - min_h] += 1
+        height_name = {}
+        for i in range(len(names)):
+            height_name[heights[i]] = names[i]
 
-        answer = []   
-        for i in range(len(ans)- 1, -1 ,-1):
-            if ans[i]:
-                answer.append(i + min_h)
+        for i in range(len(names)):
+            for j in range(1, len(names)):
+                if heights[j - 1] < heights[j]:
+                    heights[j - 1], heights[j] = heights[j], heights[j - 1]
+
+        return [height_name[height] for height in heights]          
+
         
-        for i, ans in enumerate(answer) :
-            names[i] = heights_to_names[ans] 
-
-        return names  
