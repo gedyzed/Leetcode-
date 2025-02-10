206c597ea283a77aeb28b8adfcc7bb42870a447a
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-10 15:01:33 +0300

    Time: 763 ms (5.03%) | Memory: 18.3 MB (41.17%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -6,9 +6,14 @@ class Solution:
             height_name[heights[i]] = names[i]
 
         for i in range(len(names)):
+            is_sorted = True
             for j in range(1, len(names)):
                 if heights[j - 1] < heights[j]:
                     heights[j - 1], heights[j] = heights[j], heights[j - 1]
+                    is_sorted = False
+                    
+            if is_sorted:
+                break        
 
         return [height_name[height] for height in heights]          
 
