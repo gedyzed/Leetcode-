af4f1b9af062f2cd01e75699c8d54e243e6bb950
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-11 10:43:35 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (82.22%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+
+        citations.sort()
+        h_index = 0
+        for i in range(len(citations)):
+            if citations[i] >= len(citations) - i:
+                h_index = max(h_index, len(citations) - i)
+        return h_index        
+
+
+
+        
