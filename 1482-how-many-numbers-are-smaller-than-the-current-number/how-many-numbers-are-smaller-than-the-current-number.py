b750167dcac02d16d62320801c0481b799e0b533
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-11 15:12:51 +0300

    Time: 4 ms (57.01%) | Memory: 18 MB (25.10%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,35 +1,15 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
 
-        nums_ = sorted(nums)
-        count = {}
-        result = []
-
-        for i in range(len(nums_)):
-            if nums_[i] not in count:
-                count[nums_[i]] = i
-        for i in range(len(nums)):
-            result.append(count[nums[i]])    
-
-        return result        
-
-
-
-
-
-
-        count = Counter(nums)
-
-        nums_ = list(set(nums))
+        nums_ = [num for num in nums]
         nums_.sort()
-        min_elements = defaultdict(int)
-        min_elements[nums_[0]] = count[nums_[0]]
+        num_smaller, count = {}, 1
+        num_smaller[nums_[0]] = 0
 
-        for i in range(1,len(nums_)):
-            min_elements[nums_[i]] += count[nums_[i]] + min_elements[nums_[i - 1]]
+        for i in range(1, len(nums_)):
+            if nums_[i] != nums_[i - 1]:
+                num_smaller[nums_[i]] = count
+            count += 1
 
-        answer = []  
-        for num in nums:
-            answer.append(min_elements[num] - count[num])  
-
-        return answer         
+        return [num_smaller[num] for num in nums ]
+        
