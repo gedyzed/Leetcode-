3e7c0ef5df882257865e55adc623ed3c9f3edf33
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-11 15:20:52 +0300

    Time: 3 ms (69.22%) | Memory: 18 MB (25.10%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,15 +1,24 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
 
-        nums_ = [num for num in nums]
-        nums_.sort()
-        num_smaller, count = {}, 1
-        num_smaller[nums_[0]] = 0
+        def countSorting(nums):
+            countArray = [0] * 101
 
-        for i in range(1, len(nums_)):
-            if nums_[i] != nums_[i - 1]:
-                num_smaller[nums_[i]] = count
-            count += 1
+            for num in nums:
+                countArray[num] += 1
+
+            result = [] 
+            for i, count in enumerate(countArray):
+                result.extend([i] * count)  
+
+            return result     
+
+        nums_ = countSorting(nums)
+        num_smaller = {}
+
+        for i in range(len(nums_)):
+            if nums_[i] not in num_smaller:
+                num_smaller[nums_[i]] = i
 
         return [num_smaller[num] for num in nums ]
         
