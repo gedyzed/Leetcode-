6d1db0b5aa0251dbcf954687afc035dcc6703705
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-13 11:44:16 +0300

    Time: 7 ms (7.28%) | Memory: 18 MB (25.89%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -1,11 +1,25 @@
+from typing import List
+
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        
+        def countingSort(lst):
+            max_, min_ = max(lst), min(lst)
+            count_array = [0] * (max_ - min_ + 1)
 
-        seats, students = sorted(seats), sorted(students)
+            for num in lst:
+                count_array[num - min_] += 1
+
+            sorted_lst = [] 
+            for i, count in enumerate(count_array):
+                sorted_lst.extend([i + min_] * count) 
+
+            return sorted_lst    
+
+        seats, students = countingSort(seats), countingSort(students)
         moves = 0
 
         for i in range(len(seats)):
             moves += abs(seats[i] - students[i])
 
-        return moves    
-        
+        return moves
