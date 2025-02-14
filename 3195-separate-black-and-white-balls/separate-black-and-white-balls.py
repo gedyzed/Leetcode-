fb2e014973bdbedf7d0fef2e319bf59eab9b7472
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-14 15:30:06 +0300

    Time: 75 ms (29.23%) | Memory: 18 MB (79.87%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,15 +1,13 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
 
-        s = list(s)
         ones = swaps = 0
         for i in range(len(s)):
             if s[i] == '1':
                 ones += 1
-
             elif ones and s[i] == '0':
                 swaps += ones
-
+                
         return swaps         
                  
 
