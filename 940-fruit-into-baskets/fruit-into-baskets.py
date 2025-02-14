1752e550e201f65e56eaf79b0e7c51080c3d94df
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-14 18:32:03 +0300

    Time: 187 ms (53.78%) | Memory: 23.6 MB (76.58%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,23 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        max_fruits = left = treeCount = 0
-        trees = defaultdict(int)
 
-        for right in range(len(fruits)):
-            if trees[fruits[right]] == 0:
-                treeCount += 1
+        left = max_len = 0
+        fruits_c = defaultdict(int)
+        for right, fruit in enumerate(fruits):
+            fruits_c[fruit] += 1
 
-            trees[fruits[right]] += 1 
+            while len(fruits_c) > 2:
+                fruit = fruits[left]
+                fruits_c[fruit] -= 1
+                if not fruits_c[fruit]:
+                    del fruits_c[fruit]
+                left += 1    
 
-            while treeCount > 2:
-                trees[fruits[left]] -= 1 
-                if trees[fruits[left]] == 0:
-                    treeCount -= 1
-                left += 1        
-                
-            max_fruits = max(max_fruits, right - left + 1)   
-
-        return max_fruits        
-
+            max_len = max(max_len, right - left + 1)     
+        return max_len     
 
         
