aa867b1245e67005ecd0adf2c75a52c3036164ba
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-14 22:38:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (70.34%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,34 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        left, right = m - 1, n - 1
-        i = n + m - 1
-        while left >= 0 and right >= 0:   
+        right = 0
+        for left in range(m, len(nums1)):
+            nums1[left] = nums2[right]
+            right += 1
 
-            if nums1[left] >= nums2[right]:
-                nums1[i] = nums1[left] 
-                left -= 1
-            else:
-                nums1[i] = nums2[right] 
-                right -= 1  
-            i -= 1   
-
-        while right >= 0:
-            nums1[i] = nums2[right]  
-            right -= 1
-            i -= 1    
-
-           
-
-
-
-
-
-
-   
-
-        
-
+        nums1.sort()    
 
 
         
