e55786640cee254c94d0641dab40e42ef573184f
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-18 22:10:48 +0300

    Time: 3 ms (70.45%) | Memory: 17.6 MB (98.46%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -1,10 +1,18 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
+
+        def findK(n):
+            max_index = 0
+            for i in range(1, n):
+                if arr[i] > arr[max_index]:
+                    max_index = i
+            return max_index
+
         flips = []
         curr_size = len(arr)
         
         while curr_size > 1:
-            k = self.findK(arr, curr_size)
+            k = findK(curr_size)
             if k != curr_size - 1:
                 if k != 0:  
                     flips.append(k + 1)
@@ -17,9 +25,12 @@ class Solution:
         
         return flips
 
-    def findK(self, arr: List[int], n: int) -> int:
-        max_index = 0
-        for i in range(1, n):
-            if arr[i] > arr[max_index]:
-                max_index = i
-        return max_index
+
+
+
+
+
+
+
+
+        
