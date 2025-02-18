2c5a53829f2a97a07ce8910cc77b6794d263ec37
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-18 16:16:41 +0300

    Time: 37 ms (25.05%) | Memory: 18.7 MB (77.64%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,14 +1,18 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
 
-        left = count = presum = 0
-        sum_hash = defaultdict(int)
-        sum_hash[0] = 1
-        for right in range(len(nums)):
-            
-            presum += nums[right]
-            count += sum_hash[presum - goal]
-            sum_hash[presum] += 1
+        def helper(goal):    
+            left = count = presum = 0
+            for right in range(len(nums)):
+                presum += nums[right]
 
-        return count    
+                while presum > goal and left <= right :
+                    presum -= nums[left]
+                    left += 1
+
+                count += right - left + 1
+            
+            return count  
+              
+        return helper(goal) - helper(goal - 1)    
         
