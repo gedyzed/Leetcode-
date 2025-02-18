1cc9939cc19eaed2332803d8efe287ac42b48518
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-18 11:15:15 +0300

    Time: 33 ms (37.77%) | Memory: 21.1 MB (69.62%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -2,16 +2,16 @@ class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
 
         left = presum = count = 0
-        sum_hash = defaultdict(int)
-        sum_hash[0] = 1
+        mod_hash = defaultdict(int)
+        mod_hash[0] = 1
         for right in range(len(nums)):
             presum += nums[right]
-            x  = presum % k
+            x = presum % k
     
-            if x in sum_hash:
-                count += sum_hash[x]  
+            if x in mod_hash:
+                count += mod_hash[x]  
 
-            sum_hash[presum % k] += 1  
+            mod_hash[presum % k] += 1  
 
         return count     
 
