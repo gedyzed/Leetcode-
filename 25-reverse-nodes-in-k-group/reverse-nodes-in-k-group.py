a71d6ef38bf8a5e684185311e62515dcbe1a05d2
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-21 14:38:54 +0300

    Time: 4 ms (15.04%) | Memory: 18.6 MB (60.76%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        def reverse(curr, last):
+            next, prev = last.next, None
+            temp_head = curr
+            count = 0
+            
+            while curr and count < k:
+                temp = curr
+                curr = curr.next
+                temp.next = prev
+                prev = temp
+                count += 1
+
+            # Connect the remaining elements  
+            temp_head.next = next  
+            return prev, temp_head  
+
+        if k == 1:
+            return head
+
+        count, curr = 0, None
+        last = head
+        new_head = None
+
+        # First, check if there are at least k nodes remaining
+        temp = last
+        for _ in range(k):
+            if not temp:
+                return head  # Not enough nodes to reverse
+            temp = temp.next
+
+        while last:
+            count += 1
+
+            if count == k:
+                temp = last.next
+                if not curr:
+                    new_group, prev_tail = reverse(head, last)
+                    new_head = new_group
+                else:  
+                    new_group, prev_tail = reverse(curr.next, last)
+                    curr.next = new_group
+
+                curr = prev_tail
+                last = temp
+                count = 0
+            else:       
+                last = last.next  
+
+        return new_head if new_head else head
