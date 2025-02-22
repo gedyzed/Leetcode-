99bf85527ea1b9d4a299d1efb89414d7d3fa6c7d
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-22 21:16:54 +0300

    Time: 1 ms (53.36%) | Memory: 17.7 MB (95.15%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,26 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        oper = {'-', '+'}  
+        lower_limit = -2 ** 31
+        upper_limit = 2 ** 31 - 1
+
+        result, s = [], s.strip()  
+
+        for i, char in enumerate(s):
+            if char in oper and not result:  
+                result.append(char)
+            elif char.isdigit():
+                result.append(char)
+            else:
+                break 
+
+        if len(result) == 1 and result[0] in oper:
+            return 0  
+
+        num = int("".join(result)) if result else 0
+        if num < lower_limit:
+            return lower_limit
+        if num > upper_limit:
+            return upper_limit
+
+        return num
