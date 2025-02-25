fe31d2b2ff796d7c488e265e35586846068e860e
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-25 13:39:46 +0300

    Time: 1 ms (82.75%) | Memory: 17.8 MB (85.04%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,25 +9,19 @@ class Solution:
         dummy = ListNode(0)
         curr, carry = dummy, 0
         while l1 or l2 or carry:
+
             sum = carry
-            if l1:
-                sum += l1.val
-            if l2:
-                sum += l2.val   
+            sum = sum + l1.val if l1 else sum
+            sum = sum + l2.val if l2 else sum          
 
             digit = sum % 10 
             node = ListNode(digit)
             curr.next = node
             curr = node
-            if sum > 9:
-                carry = 1
-            else:
-                carry = 0    
-
-
+            
+            carry = 1 if sum > 9 else 0
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-
 
 
         return dummy.next    
