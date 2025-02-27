3a6e5baf2090261e1dc4e9f7d1d4262c4c53c636
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-02-27 19:31:18 +0300

    Time: 64 ms (62.77%) | Memory: 28.3 MB (99.03%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
 
-        
+        # find the max_sum
         presum = 0
         max_sum = float('-inf')
         for right in range(len(nums)):
@@ -10,6 +10,7 @@ class Solution:
             presum += nums[right]
             max_sum = max(max_sum, presum)
         
+        # find the min_sum
         presum = 0
         min_sum = float('inf')
         for right in range(len(nums)):
@@ -18,6 +19,7 @@ class Solution:
             presum += nums[right] 
             min_sum = min(min_sum, presum)   
 
+        # return the max of the two abs
         return max(max_sum, abs(min_sum))    
 
 
