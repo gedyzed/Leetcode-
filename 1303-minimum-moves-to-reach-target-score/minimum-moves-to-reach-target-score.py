ebcfdeba0aa217a04b29b15ad120182766492cae
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-04 19:45:16 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.73%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
@@ -2,7 +2,7 @@ class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
 
         
-        steps, num = 0, 1
+        steps  = 0
         while maxDoubles and target > 1:
             num = target // 2
             steps += target - num * 2 + 1
