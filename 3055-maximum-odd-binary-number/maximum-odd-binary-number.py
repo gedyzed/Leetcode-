44276397f868c9edc69038076814e5aaaf02716e
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-04 18:18:21 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.52%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
 
+        # only keep one "1" to the right-end
+        # keep others at the beginning
+
         counter = Counter(s)
-        ones = counter["1"] - 1
-        zeros = counter["0"]
+        ones, zeros = counter["1"] - 1, counter["0"] 
         result = ["1" * ones] + ["0" * zeros] + ["1"]
+        
         return "".join(result)
 
 
