645c0ac16fd65164e4778c7986da13550ef7fa2f
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-04 18:27:32 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (72.43%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+
+        city = [(a - b, a, b) for a, b in costs]
+        city.sort() 
+    
+        n = len(costs) // 2
+        ans = 0
+        for i in range(len(city)):
+            if i < n:
+                ans += city[i][1]
+            else:
+                ans += city[i][2]  
+
+        return ans          
+
+
+        
+
+
+
+
+
+       
+    
+  
+
+      
+        
