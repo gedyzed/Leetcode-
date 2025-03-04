f707b9a53763cb74632357551e7ac95238589ead
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-04 17:55:26 +0300

    Time: 3 ms (78.46%) | Memory: 22 MB (9.81%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,26 +1,23 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
 
-        count_five = count_ten = 0
+        bills_count = defaultdict(int)
         for bill in bills:
-            if bill == 5:
-                count_five += 1
-            elif bill == 10:
-                count_ten += 1
-                if not count_five :
+            if bill == 10:
+                if not bills_count[5]:
                     return False
-                count_five -= 1
-            else:
-                if count_five and count_ten:
-                    count_five -= 1
-                    count_ten -= 1
-                elif count_five >= 3:
-                    count_five -= 3
-                else:
-                    return False                    
-      
+                bills_count[5] -= 1    
+
+            elif bill == 20:
+                if bills_count[10] and bills_count[5]:
+                    bills_count[10] -= 1
+                    bills_count[5] -= 1
+                elif bills_count[5] >= 3:
+                    bills_count[5] -= 3    
+                else:    
+                    return False
+
+            bills_count[bill] += 1
+            
         return True                 
-                    
-
-
-        
+                          
