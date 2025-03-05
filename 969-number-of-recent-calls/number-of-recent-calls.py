6b1c37f1aa906d1bba8a90965d9aba9130bc6d82
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-05 19:05:59 +0300

    Time: 16 ms (98.96%) | Memory: 23.1 MB (76.65%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -1,21 +1,17 @@
 class RecentCounter:
 
     def __init__(self):
-
-        self.queue = deque()  
-
+        self.queue = deque()
+    
     def ping(self, t: int) -> int:
+      
 
-        self.queue.append(t)
-        while  (t- self.queue[0]) > 3000 :
-            self.queue.popleft()   
+        while self.queue and self.queue[0] < t - 3000 :
+            self.queue.popleft()
 
-        return len(self.queue)
+        self.queue.append(t)   
+        return len(self.queue)     
 
-
-
-
-        
 
         
 
