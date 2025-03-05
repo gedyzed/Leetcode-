6e36085c158eb8f86f7fbb72dad153a09e480da0
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-05 22:44:25 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.02%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.py
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -3,10 +3,11 @@ class Solution:
 
         stack = []
         for log in logs:
-            if log == "../":
-                if stack: stack.pop()
-            elif log != "./":
-                stack.append(log)  
-                  
+            if log == "../" and stack:
+                stack.pop()
+            elif log != "./" and log != '../':
+                stack.append(log) 
+                
         return len(stack)        
+
         
