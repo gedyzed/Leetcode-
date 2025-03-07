6e1b5d16f5bfea4efa1fade940031de36e0f8cef
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-07 17:48:28 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.33%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,21 +1,25 @@
 class MyQueue:
 
     def __init__(self):
-        self.queue = []
-        
+        self.stack1 = []
+        self.stack2 = []
 
     def push(self, x: int) -> None:
-        self.queue.append(x)
-        
+
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(x)
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
 
     def pop(self) -> int:
-        return self.queue.pop(0)
+        return self.stack1.pop(-1)
 
     def peek(self) -> int: 
-        return self.queue[0] 
+        return self.stack1[-1] 
         
     def empty(self) -> bool:
-        if self.queue:
+        if self.stack1:
             return False
         return True    
         
