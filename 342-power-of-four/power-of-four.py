28a5c21c536f5349e49a683d14cbdb13a8d2f06a
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-11 18:35:14 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (41.14%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -3,8 +3,20 @@ class Solution:
 
         if n <= 0:
             return False
+        if n == 1:
+            return True
+        if n % 4 != 0:
+            return False     
 
-        e = math.log(abs(n), 4)
-        return e == int(e)
+        return self.isPowerOfFour(n // 4)    
+
+
+
+
+
+
+
+
+   
 
         
