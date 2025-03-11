69966fc59942bd211222b9c4d183e34dd6b0e8d9
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-11 13:04:49 +0300

    Time: 823 ms (71.36%) | Memory: 44.8 MB (15.45%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -3,18 +3,14 @@ class Solution:
 
 
         happiness.sort(reverse=True)
-        happiness_value = 0
-        decrement = i = 0
+        happiness_sum = decrement = i = 0
 
-        while k and i < len(happiness):
-            happy = happiness[i]
-            if happy < decrement:
-                break
+        for i in range(k):
 
-            happiness_value += happy - decrement
-            decrement += 1  
-            k -= 1
-            i += 1
+            happiness_sum += max(happiness[i] - decrement, 0)
+            decrement += 1
 
-        return happiness_value        
+        return happiness_sum    
+
+       
         
