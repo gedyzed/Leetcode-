9b12ed261d3de4331d0710f7bc7b85a71c19c823
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-13 19:07:56 +0300

    Time: 207 ms (8.58%) | Memory: 62.7 MB (5.49%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -6,21 +6,19 @@
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        dummy = ListNode(0, head)
-        curr, max_ = dummy, 0
-        
         def removeNode(prev, curr):
 
             if not curr:
                 return 0
             
-            max_ = max(curr.val, removeNode(prev.next, curr.next))
-            if curr.val < max_:
-                prev.next = prev.next.next
+            max_right = removeNode(prev.next, curr.next)
+            if curr.val < max_right:
+                prev.next = curr.next
 
-            return max_     
+            return max(curr.val, max_right)    
 
-        removeNode(curr, curr.next)
+        dummy = ListNode(0, head)  
+        removeNode(dummy, head)
         return dummy.next
 
         
