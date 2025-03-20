00a4ff1789cb0015d7590758e6c745b04f4b6d3f
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-20 19:22:34 +0300

    Time: 7422 ms (5.18%) | Memory: 17.8 MB (47.75%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -0,0 +1,25 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+
+        def backtrack(idx):
+
+            if idx >= len(cookies):
+                self.min = min(self.min, max(childs))
+                return 
+
+            if self.min < max(childs):
+                return    
+
+            for i in range(k):
+                childs[i] += cookies[idx]
+                backtrack(idx + 1)
+                childs[i] -= cookies[idx]
+
+        
+        self.min = float('inf')
+        self.res = max(cookies)
+        childs = [0] * k   
+        backtrack(0)  
+        return self.min   
+
+        
