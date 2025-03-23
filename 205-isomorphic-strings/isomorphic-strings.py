a6fe64596488dfe5b3c5a48ac8b8ffbacb22fa5d
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-23 23:55:52 +0300

    Time: 7 ms (57.06%) | Memory: 17.8 MB (80.24%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_index_s = {}
+        char_index_t = {}
+
+        for i in range(len(s)):
+            if s[i] not in char_index_s:
+                char_index_s[s[i]] = i
+
+            if t[i] not in char_index_t:
+                char_index_t[t[i]] = i
+            
+            if char_index_s[s[i]] != char_index_t[t[i]]:
+                return False
+
+        return True
