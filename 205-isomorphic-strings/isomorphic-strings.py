ee5bcd640ddc15818b2441d991d579ac73b9352e
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-03-24 09:21:01 +0300

    Time: 10 ms (20.23%) | Memory: 18.1 MB (37.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,16 +1,35 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        char_index_s = {}
-        char_index_t = {}
+
+        map_st = {}
+        map_ts = {}
+        for i in range(len(s)):
+            if s[i] not in map_st and t[i] not in map_ts :
+                map_st[s[i]] = t[i]
+                map_ts[t[i]] = s[i]
 
         for i in range(len(s)):
-            if s[i] not in char_index_s:
-                char_index_s[s[i]] = i
-
-            if t[i] not in char_index_t:
-                char_index_t[t[i]] = i
-            
-            if char_index_s[s[i]] != char_index_t[t[i]]:
+           if s[i] not in map_st or map_st[s[i]] != t[i]:
                 return False
+        return True        
 
-        return True
+            
+
+            
+    
+
+        
+
+
+
+     
+
+
+
+
+
+
+        
+
+
+        
