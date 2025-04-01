6694ac3a86c73f5d9f37b83a7e76ea8d29c6b0e6
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-04-01 18:38:34 +0300

    Time: 53 ms (78.50%) | Memory: 19.8 MB (47.39%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
@@ -7,16 +7,14 @@ class Solution:
                 res.append("".join(ans))
                 return
 
-            if (zeros <= n - 1 and not ans) or (ans[-1] == '1'):
+            if not ans or ans[-1] == '1':
                 ans.append("0")  
                 backtrack(ones, zeros + 1)  
                 ans.pop()
 
-            if ones < n :
-                ans.append("1")   
-                backtrack(ones + 1, zeros) 
-                ans.pop()
-
+            ans.append("1")   
+            backtrack(ones + 1, zeros) 
+            ans.pop()
 
         res, ans = [], []
         backtrack(0, 0)     
