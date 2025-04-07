07ac681152b77ad141846a2d1e92b12957295035
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-04-07 19:52:51 +0300

    Time: 11 ms (77.19%) | Memory: 21 MB (16.80%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+
+        if n == 1:
+            return 1
+
+        trusts = set()
+        not_trust = defaultdict(int)
+        for p1, p2 in trust:
+            trusts.add(p1)
+
+            if p1 in not_trust:
+                not_trust.pop(p1)
+            
+            if p2 not in trusts:
+                not_trust[p2] += 1
+
+        judge, count = list(not_trust.keys()), list(not_trust.values())
+        if not judge:
+            return - 1
+        return judge[0] if count[0] == n - 1 else -1
+     
+          
+
