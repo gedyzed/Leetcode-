41a850e3567205d1a5f9e39fcc3cd1302ed3d097
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-04-09 10:12:28 +0300

    Time: 3 ms (74.07%) | Memory: 18 MB (23.59%) - LeetSync

--- 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
@@ -2,12 +2,9 @@ class Solution:
     def splitString(self, s: str) -> bool:
 
         def backtrack(idx):
-            if idx >= len(s):
-                for i in range(1, len(curr)):
-                    if curr[i - 1] - curr[i] != 1:
-                        return False
-                return len(curr) >= 2    
-
+            if idx >= len(s) :
+                return len(curr) >= 2
+      
             for i in range(idx, len(s)):
                 num = int(s[idx: i + 1])
                 if curr and curr[-1] - num != 1:
