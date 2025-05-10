815368c9f2486ca5b0fbb9d86e6521450136b729
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-05-10 22:25:24 +0300

    Time: 3 ms (89.62%) | Memory: 18.8 MB (83.54%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,19 +1,46 @@
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-   
-        provinces = 0
-        visited_cities = set()
 
-        def dfs(city): 
-            visited_cities.add(city)
-            for i in range(len(isConnected[city])):
-                if isConnected[city][i] == 1:
-                    if i not in visited_cities:
-                        dfs(i)
-        
-        for i in range(len(isConnected)):
-            if i not in visited_cities:
-                provinces += 1
-                dfs(i)
-        
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
+        n = len(isConnected)
+        cities = [i for i in range(n)]
+        rank = [0] * n
+
+        def find(x):
+            if x == cities[x]:
+                return x
+
+            cities[x] = find(cities[x])   
+            return cities[x] 
+
+        def union(x, y):
+            rootX, rootY  = find(x), find(y)  
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    cities[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    cities[rootX] = rootY
+                else:
+                    cities[rootX] = rootY
+                    rank[rootY] += 1   
+                return True
+                
+            return False         
+
+        provinces = n
+        for row in range(n):
+            for col in range(row + 1, n):
+                if isConnected[row][col] :
+                        if union(row, col):
+                            provinces -= 1
+
         return provinces
+
+
+
+        
+
+
+
+   
+       
