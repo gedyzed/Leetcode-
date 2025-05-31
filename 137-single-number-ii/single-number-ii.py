ee40ccebf5aa45035de6d547fc0d6b5a50e7faa9
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-05-31 21:31:01 +0300

    Time: 37 ms (16.55%) | Memory: 19.2 MB (52.05%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,11 +1,27 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        nums_count = Counter(nums)
-        for key in nums_count:
-            if nums_count[key] == 1:
-                return key
-                
+        ans = 0
+        for i in range(32):
+            sum = 0
+            for num in nums:
+                if num < 0:
+                    num = num & (2 ** 32 - 1)
+                sum += num >> i & 1
+
+            sum %= 3
+            ans |= sum << i
+
+        if ans >= 2 ** 31:
+            ans -= 2 ** 32
+
+
+        return ans            
+
+
+
+       
+
 
 
         
