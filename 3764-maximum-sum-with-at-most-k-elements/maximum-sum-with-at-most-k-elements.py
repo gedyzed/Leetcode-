e95cff1f28df5c0a0ed234fd5fccc1e1e2f2ab06
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-06-16 23:06:09 +0300

    Time: 183 ms (81.67%) | Memory: 45 MB (92.35%) - LeetSync

--- 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
 
-        for i in range(len(grid)):
-            grid[i].sort()
-    
         res = []
         for i, nums in enumerate(grid):
-            limit = limits[i]
-            while limit and nums:
+            grid[i].sort()
+            while limits[i] and nums:
                 res.append(nums.pop())
-                limit -= 1
+                limits[i] -= 1
 
         return sum(sorted(res, reverse=True)[:k]) if len(res) >= k else sum(res)
 
