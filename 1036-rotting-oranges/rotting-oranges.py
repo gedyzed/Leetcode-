9fcedb68bf5ce5b7e0ad576f26091ca8ac715106
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-06-17 23:02:09 +0300

    Time: 3 ms (82.18%) | Memory: 18 MB (26.43%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,70 +1,38 @@
-from collections import deque
-
-# Time complexity: O(rows * cols) -> each cell is visited at least once
-# Space complexity: O(rows * cols) -> in the worst case if all the oranges are rotten they will be added to the queue
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
-        # number of rows
-        rows = len(grid)
-        if rows == 0:  # check if grid is empty
-            return -1
-        
-        # number of columns
-        cols = len(grid[0])
-        
-        # keep track of fresh oranges
-        fresh_cnt = 0
-        
-        # queue with rotten oranges (for BFS)
-        rotten = deque()
-        
-        # visit each cell in the grid
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    # add the rotten orange coordinates to the queue
-                    rotten.append((r, c))
-                elif grid[r][c] == 1:
-                    # update fresh oranges count
-                    fresh_cnt += 1
-        
-        # keep track of minutes passed.
-        minutes_passed = 0
-        
-        # If there are rotten oranges in the queue and there are still fresh oranges in the grid keep looping
-        while rotten and fresh_cnt > 0:
 
-            # update the number of minutes passed
-            # it is safe to update the minutes by 1, since we visit oranges level by level in BFS traversal.
-            minutes_passed += 1
+        def inbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        time = fresh = 0
+        queue = deque()
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                     queue.append((r, c))   
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    row, col = dr + r, dc + c
+                    if inbound(row, col) and grid[row][col] == 1:
+                        grid[row][col] = 2
+                        queue.append((row, col))
+                        fresh -= 1
+
+            time += 1 
             
-            # process rotten oranges on the current level
-            for _ in range(len(rotten)):
-                x, y = rotten.popleft()
-                
-                # visit all the adjacent cells
-                for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-                    # calculate the coordinates of the adjacent cell
-                    xx, yy = x + dx, y + dy
-                    # ignore the cell if it is out of the grid boundary
-                    if xx < 0 or xx == rows or yy < 0 or yy == cols:
-                        continue
-                    # ignore the cell if it is empty '0' or visited before '2'
-                    if grid[xx][yy] == 0 or grid[xx][yy] == 2:
-                        continue
+        return time if not fresh else -1    
+
+
+
+                                    
                         
-                    # update the fresh oranges count
-                    fresh_cnt -= 1
-                    
-                    # mark the current fresh orange as rotten
-                    grid[xx][yy] = 2
-                    
-                    # add the current rotten to the queue
-                    rotten.append((xx, yy))
+
+
 
         
-        # return the number of minutes taken to make all the fresh oranges to be rotten
-        # return -1 if there are fresh oranges left in the grid (there were no adjacent rotten oranges to make them rotten)
-        return minutes_passed if fresh_cnt == 0 else -1
