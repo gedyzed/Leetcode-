44047a47dcf2c778a96b53b2ecba7768097d8047
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-06-18 15:56:41 +0300

    Time: 7 ms (20.45%) | Memory: 17.8 MB (91.95%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
@@ -1,31 +1,6 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-
-
-        def bfs(target, start_with_red):
-            
-            queue = deque([(0, 0, start_with_red)])
-            visited = set((0, start_with_red))
-            while queue:
-                
-                node, lvl, flag = queue.popleft()
-                if node == target:
-                    return lvl
-
-                if not flag :
-                    for nei in red[node]:
-                        if (nei, True) not in visited:
-                            queue.append((nei, lvl + 1, True))  
-                            visited.add((nei, True))
-                else:
-                    for nei in blue[node]:
-                        if (nei, False) not in visited:
-                            queue.append((nei, lvl + 1, False))
-                            visited.add((nei, False))
-                
-            return 101     
-
-                    
+                 
         red = defaultdict(list)
         blue = defaultdict(list)
 
@@ -35,13 +10,28 @@ class Solution:
         for u, v in blueEdges:
             blue[u].append(v)  
 
-        ans = []
-        for i in range(n):
-            x = min(bfs(i, False), bfs(i, True))  
-            if x == 101:
-                x = -1
-            ans.append(x)
+        queue = deque([(0, 0, None)])
+        visited = set((0, None))
+        ans = [-1 for _ in range(n)]
+        while queue:
+            
+            node, lvl, flag = queue.popleft()
+            if ans[node] == -1:
+                ans[node] = lvl
 
+         
+            if flag != "RED":
+                for nei in red[node]:
+                    if (nei, "RED") not in visited:
+                        queue.append((nei, lvl + 1, "RED"))  
+                        visited.add((nei, "RED"))
+
+            if flag != "BLUE":
+                for nei in blue[node]:
+                    if (nei, "BLUE") not in visited:
+                        queue.append((nei, lvl + 1, "BLUE"))
+                        visited.add((nei, "BLUE"))
+                
         return ans     
 
 
