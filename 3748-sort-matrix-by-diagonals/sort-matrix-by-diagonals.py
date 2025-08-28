85691691d1c30abb2a3e480736b6e85571781482
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-08-28 14:29:38 +0300

    Time: 20 ms (17.08%) | Memory: 17.9 MB (52.08%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -1,13 +1,22 @@
 class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
 
+        # extract diagonals
         n = len(grid)
         diagonals = defaultdict(list)
+        row = defaultdict(list)
+        col = defaultdict(list)
+
         for r in range(n):
             for c in range(n):
                 num = grid[r][c]
                 diagonals[r - c].append(num)
-        
+                row[r - c].append(r)
+                col[r - c].append(c)
+
+        # create ans matrix 
+        # sort the diagonals and reverse to make it suitable for pop
+        # for each daigonals go over every matrix and check
 
         ans = [[0] * n for _ in range(n)]
         for idx in diagonals:
@@ -18,8 +27,8 @@ class Solution:
                 nums.sort(reverse=True)  
 
             nums = nums[::-1]     
-            for r in range(n):
-                for c in range(n):
+            for r in row[idx]:
+                for c in col[idx]:
                     if r - c == idx:
                         ans[r][c] = nums.pop()
 
