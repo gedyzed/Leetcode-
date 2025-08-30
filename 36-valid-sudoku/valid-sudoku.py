94c6b0ce5087c96136516a310d3c0b9c1896f25a
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-08-30 23:24:17 +0300

    Time: 1 ms (88.76%) | Memory: 17.8 MB (69.70%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,37 +1,31 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        def block_check(row, col):
-            block_set = set()
-            for r in range(row, row + 3):
-                for c in range(col, col + 3):
-                    if board[r][c] != '.':
-                        if board[r][c] in block_set:
-                            return False
-                        block_set.add(board[r][c])
-            return True
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxs = defaultdict(set)
 
         for row in range(len(board)):
-            row_set = set()
             for col in range(len(board[0])):
-                if board[row][col] != '.':
-                    if board[row][col] in row_set:
-                        return False
-                    row_set.add(board[row][col])
+                num = board[row][col]
+                if num == ".":
+                    continue
 
-
-        for col in range(len(board[0])):
-            col_set = set()
-            for row in range(len(board)):
-                if board[row][col] != '.':
-                    if board[row][col] in col_set:
-                        return False
-                    col_set.add(board[row][col])
-  
-
-        for row in range(0, 9, 3):
-            for col in range(0, 9, 3):
-                if not block_check(row, col):
+                if num in rows[row]:
                     return False
 
-        return True
+                if num in cols[col]:
+                    return False
+
+                if num in boxs[(row // 3, col // 3)]:
+                    return False
+
+                rows[row].add(num)
+                cols[col].add(num)   
+                boxs[(row // 3, col // 3)].add(num)       
+
+        return True        
+
+
+
+        
