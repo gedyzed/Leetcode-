13f2e735e365e5e67e3eae2b68cc789c0c957a66
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-09-15 13:20:34 +0300

    Time: 7 ms (6.21%) | Memory: 17.9 MB (36.81%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -3,11 +3,13 @@ class Solution:
 
         text = text.split()
         words = set()
-        for bl in brokenLetters:
-            for i, word in enumerate(text):
-                if bl in word and (word, i) not in words:
-                    words.add((word, i))
 
+        for i, word in enumerate(text):
+            set_word = set(word)
+            for b_letter in brokenLetters:
+                if b_letter in set_word and (word, i) not in words:
+                    words.add((word, i))
+                    
         return len(text) - len(words)            
                    
 
