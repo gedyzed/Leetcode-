4f5d5b97f6a949046ae5b7a83e8df7fbea13c8cf
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-09-17 23:02:02 +0300

    Time: 0 ms (100.00%) | Memory: 18.4 MB (82.92%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -3,14 +3,10 @@ class Solution:
 
         left = 0
         for right in range(len(nums)):
-            if left != right and nums[right] % 2 == 0:
+            if nums[right] % 2 == 0:
                 nums[left], nums[right] = nums[right], nums[left]   
                 left += 1
-                
-            if nums[left] % 2 == 0:
-                left += 1
-
-
+           
         return nums          
        
         
