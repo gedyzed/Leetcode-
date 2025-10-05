538f67c4136d90242285b8a57d7f2c1ea61073b8
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-10-05 20:35:07 +0300

    Time: 476 ms (16.63%) | Memory: 128.9 MB (10.71%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,5 +1,23 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+
+        memo = defaultdict(int)
+        def dp(left, right):
+
+            idx = (left, right)
+            if idx in memo:
+                return memo[idx]
+
+            if left == right:
+                return 0
+            
+            res = max(piles[left], piles[right]) + max(dp(left + 1, right), dp(left, right - 1))
+            memo[idx] = res
+            return res
+
+        sum_ = dp(0, len(piles) - 1)
+        return sum_ > sum(piles) // 2
+        
+
         
         
