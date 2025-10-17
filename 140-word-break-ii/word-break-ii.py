5655f038a05cd05716079e81181d1e8516d52ce2
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-10-17 22:33:23 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.69%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        root = TrieNode()
+        for word in wordDict:
+            cur = root
+            for c in word:
+                if c not in cur.children:
+                    cur.children[c] = TrieNode()
+                cur = cur.children[c]
+            cur.is_word = True
+        
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            if i == 0 or dp[i - 1]:
+                cur = root
+                for j in range(i, len(s)):
+                    c = s[j]
+                    if c not in cur.children:
+                        break
+                    cur = cur.children[c]
+                    if cur.is_word:
+                        dp[j] = True
+        
+        if not dp[-1]:
+            return []
+        
+        def backtrack(idx):
+            
+            if idx == len(s):
+                ans.append(" ".join(words))
+                return 
+            
+            for i in range(idx + 1, len(s) + 1):
+                cur_word = s[idx: i]
+                if cur_word in wordDict:
+                    words.append(cur_word)
+                    backtrack(i)
+                    words.pop()
+               
+        words, ans = [], [] 
+        backtrack(0)
+        return ans
+
+        
