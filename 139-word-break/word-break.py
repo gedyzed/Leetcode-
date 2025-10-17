d1d59e7a1eff4ecabecd9bd0dda510cbbdd653d2
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-10-17 18:30:36 +0300

    Time: 23 ms (5.42%) | Memory: 18.6 MB (5.81%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,12 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.is_end = True
+    
+    def search(self, word) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        
+        return cur.is_end
+   
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        trie = Trie()
+        for word in wordDict:
+            trie.insert(word)
 
         word_dicts = set(wordDict)
         memo = [False] * (len(s) + 1)
         memo[-1] = True
         for start in range(len(s) - 1, -1, -1):
             for end in range(start + 1, len(s) + 1):
-                if s[start:end] in word_dicts and memo[end]:
+                if trie.search(s[start:end]) and memo[end]:
                     memo[start] = True
                     break
         
