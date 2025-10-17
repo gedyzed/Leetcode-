fe9cf9225756edfbe199ded46f7bd4366f355475
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-10-17 18:41:50 +0300

    Time: 0 ms (100.00%) | Memory: 18.7 MB (5.81%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,61 +1,31 @@
 class TrieNode:
     def __init__(self):
-        self.is_end = False
+        self.is_word = False
         self.children = {}
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    
-    def insert(self, word) -> None:
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
-        cur.is_end = True
-    
-    def search(self, word) -> bool:
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                return False
-            cur = cur.children[c]
-        
-        return cur.is_end
-   
+  
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
-        trie = Trie()
+        root = TrieNode()
         for word in wordDict:
-            trie.insert(word)
-
-        word_dicts = set(wordDict)
-        memo = [False] * (len(s) + 1)
-        memo[-1] = True
-        for start in range(len(s) - 1, -1, -1):
-            for end in range(start + 1, len(s) + 1):
-                if trie.search(s[start:end]) and memo[end]:
-                    memo[start] = True
-                    break
+            cur = root
+            for c in word:
+                if c not in cur.children:
+                    cur.children[c] = TrieNode()
+                cur = cur.children[c]
+            cur.is_word = True
         
-        return memo[0]
-
-
-
-
-
-
-
-
-                
-
-
-
-
-            
-           
-
-            
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            if i == 0 or dp[i - 1]:
+                cur = root
+                for j in range(i, len(s)):
+                    c = s[j]
+                    if c not in cur.children:
+                        break
+                    cur = cur.children[c]
+                    if cur.is_word:
+                        dp[j] = True
+        
+        return dp[-1]
 
