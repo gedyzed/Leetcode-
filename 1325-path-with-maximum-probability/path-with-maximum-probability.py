0db5ab075ab16146359f6188c5c63861651a845d
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-10-22 22:05:09 +0300

    Time: 128 ms (30.49%) | Memory: 28.1 MB (80.90%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+
+        graph = defaultdict(list)
+        for i in range(len(edges)):
+            u, v = edges[i]
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
+
+        
+        def dijkstra(n, start_node):
+            distances = {node: float('-inf') for node in range(n)}
+            distances[start_node] = -1
+            heap = [(-1, start_node)]
+
+            while heap:
+                cur_dist, cur_node = heappop(heap)
+                for nei, weight in graph[cur_node]:
+                    new_dist = -cur_dist * weight
+                    if new_dist > distances[nei]:
+                        distances[nei]= new_dist
+                        heappush(heap, (-new_dist, nei))
+            
+            return distances
+
+        distances = dijkstra(n, start_node)
+        ans = distances[end_node]
+        return ans if ans != float('-inf') else 0
