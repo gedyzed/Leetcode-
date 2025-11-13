98a287d9e0e350d8a43c6ba59df18d704e1e3714
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-11-13 17:43:59 +0300

    Time: 1 ms (30.82%) | Memory: 17.7 MB (70.40%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+
+        def pow_(x, n, m):
+            result, exp = 1, n
+            x = x % m
+            while exp > 0:
+                if exp & 1:
+                    result *= x
+                x = x * x % m
+                exp >>= 1
+            
+            return result
+
+        mod = 10 ** 9 + 7
+        ans, exp = 0, n // 2
+        if n & 1:
+            ans = pow_(4, exp, mod) * pow_(5, exp + 1, mod)
+        else:
+            ans = pow_(4, exp, mod) * pow_(5 , exp, mod)
+        
+        return ans % mod
+            
