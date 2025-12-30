fc8d2cbd89c5999c14b08d1e0de6592662ab36da
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-12-30 12:45:37 +0300

    Time: 30 ms (18.65%) | Memory: 18.4 MB (98.18%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -3,16 +3,14 @@ class Solution:
 
         i = 0
         while i < len(nums):
-            if nums[i] - 1 != i and nums[nums[i] - 1] != nums[i]:
+            if nums[i] != (i + 1) and nums[nums[i] - 1] != nums[i]:
                 idx = nums[i] - 1
-                nums[i], nums[idx] = nums[idx], nums[i]  
-            else:    
+                nums[i], nums[idx] = nums[idx], nums[i]
+            else:
                 i += 1
 
         for i in range(len(nums)):
             if nums[i] - 1 != i:
                 return [nums[i], i + 1]
-              
-
-        return []        
-        
+      
+   
