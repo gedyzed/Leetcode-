a5b5d4d31cb00765206006c9d478e769c7dcc4d5
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2026-02-22 21:19:15 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (31.55%) - LeetSync

--- 899-binary-gap/binary-gap.py
+++ 899-binary-gap/binary-gap.py
@@ -1,24 +1,14 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        b_nums = bin(n)[2:]
-        left = right = max_len = 0
 
-        for right in range(len(b_nums)):
-            if b_nums[right] == '1' and b_nums[left] == '1':
-                max_len = max(max_len, right - left)
-                left = right
-            elif b_nums[right] == '1' and b_nums[left] != '1':
-                left = right
-
-        return max_len        
-
-
-
-
-
-
-
-
-
-
+        left, ans = -1, 0
+        for i in range(32):
+            bit = n & 1
+            n = n >> 1
+            if bit and left == -1:
+                left = i
+            elif bit:
+                ans = max(ans, i - left)
+                left = i
         
+        return ans
